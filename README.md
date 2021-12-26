44fcc6e8a48b3c31944f63b90b6272104363f925
Cowleyfitcode <65091695+Mcowley1@users.noreply.github.com>
2021-12-25 21:07:18 -0500

    landing page logo

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # cowleyfit-program1
+
+## Summery 
+To create a website for clients to check out their own program on a desktop or mobile
+
+## Tools
+> 1. React
+> 2. Javascript
+> 3. Css
+> 4. html
+> 5. bootstrap
---
+++ src/App.js
@@ -0,0 +1,23 @@
+import logo from './Clogo.png';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Welcome to your program
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
